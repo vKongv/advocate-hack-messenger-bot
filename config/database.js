239f67218ec35503ab64748af6d1a7998733ac92
Kong <mgongzai@gmail.com>
2017-07-28 00:12:58 +0800

    add sql connection, some minor formatting

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+  'connection': {
+    'host': 'advocate.cndbzy7kuhsi.ap-southeast-1.rds.amazonaws.com',
+    'user': 'joel',
+    'password': 'gdpjoel123'
+  },
+  'database': 'advocate',
+  'users_table': 'report'
+};
