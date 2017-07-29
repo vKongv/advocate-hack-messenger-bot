f78d1f5e2e3201f5f00e59703b1bcf409801ba6a
Kong <mgongzai@gmail.com>
2017-07-29 22:09:32 +0800

    add posting function for moderator

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const
     reportDb = require('./db/report.js');
 
 const REPORT_RESPONSE_MESSAGE = "\n\nCan you please provide us the following details: \n - Time \n - Date of incident \n - Location \n - Description of event. \n\nAlso, Upload as many pictures as necessary. \n\nDo provide us your mobile number in case we need to contact you for further details.  \n\n** Disclaimer: \nPlease be assured that the information you provide will not be published publicly but will be handled only by relevant authorities.";
+const DEFAULT_POST_IMAGE = "http://copyrightuser.org/wp-content/uploads/2013/04/newsreporting.jpg";
 
 var app = express();
 app.set('port', process.env.PORT || 5000);
@@ -257,8 +258,16 @@ var receivedMessage = async (function(event) {
         sendTextMessage(senderID, "Quick reply tapped");
         return;
     }
-    //get user current status
-    var userStatus = await(getUserCurrentState(senderID));
+
+    //Get user details from db
+    var dbUsers = await(userDb.getUser(senderID));
+    var dbUser = {};
+    if (dbUsers.length > 0) {
+        dbUser = dbUsers[0];
+    } else {
+        await(userDb.insertUser(senderId));
+        dbUser = await(userDb.getUser(senderID))[0];
+    }
     
     if (messageText) {
         // If we receive a text message, check to see if it matches any special
@@ -266,7 +275,7 @@ var receivedMessage = async (function(event) {
         // the text we received.
         var textChecker = messageText.toLowerCase();
 
-        if (userStatus > 0) {
+        if (Object.keys(dbUser).length > 0 && dbUser.isReporting) {
             switch(textChecker) {
                 case "end": 
                     userDb.updateUserState(senderID, 0);
@@ -280,123 +289,161 @@ var receivedMessage = async (function(event) {
                         "I'm here to listen.",
                         "Continue.",
                     ];
-                    messageDb.insertMessage(userStatus, textChecker, messageDb.TYPE_TEXT);
+                    messageDb.insertMessage(userStatus, messageText, messageDb.TYPE_TEXT);
                     var min = Math.ceil(0);
                     var max = Math.floor(msgReplied.length);
                     const msgIndex = Math.floor(Math.random() * (max - min + 1)) + min;
                     sendTextMessage(senderID, msgReplied[msgIndex]);
                     break;
             };
-            
-        } else {
-            switch (textChecker) {
-                case 'list':
-                    sendList(senderID);
-                    break;
-                
-                case 'image':
-                    sendImageMessage(senderID);
-                    break;
-                
-                case 'gif':
-                    sendGifMessage(senderID);
-                    break;
-                
-                case 'audio':
-                    sendAudioMessage(senderID);
-                    break;
-                
-                case 'video':
-                    sendVideo(senderID);
-                    break;
-                
-                case 'file':
-                    sendFileMessage(senderID);
-                    break;
-                
-                case 'menu':
-                    showMenu(senderID);
-                    break;
-                
-                case 'generic':
-                    sendGenericMessage(senderID);
-                    break;
-                
-                case 'more picture': 
-                    sendMultipleImages(senderID);
-                    break;
-
-                case 'event':
-                    sendTextMessage(senderID, "https://www.facebook.com/events/419524075069645/");
-                    break;
-                
-                case 'receipt':
-                    sendReceiptMessage(senderID);
-                    break;
-                
-                case 'quick reply':
-                    sendQuickReply(senderID);
-                    break;        
-                
-                case 'read receipt':
-                    sendReadReceipt(senderID);
-                    break;        
-                
-                case 'typing on':
-                    sendTypingOn(senderID);
-                    break;        
-                
-                case 'typing off':
-                    sendTypingOff(senderID);
-                    break;        
-                
-                case 'account linking':
-                    sendAccountLinking(senderID);
-                    break;
-                
-                case 'report':
-                    console.log(event.message);
-                    forwardMessage(senderID, event.message);
-                    break;
-                
-                case 'show':
-                    sendLatestPost(senderID);
-                    break;
-
-                case 'show report':
-                    sendLatestReport(senderID);
-                    break;
-                
-                case 'hey':
-                    getUserInfo(senderID).then(
-                        function (response) {
-                            sendTextMessage(senderID, "hey " + response["first_name"]);
+            return;
+        } else if (dbUser.role === userDb.ROLE_MODERATOR ) {
+            if (dbUser.isPosting) {
+                var postsDetails = postDb.getPostDetails(dbUser.isPosting);
+                if (postDetails.length > 0) {
+                    var post = postDetails[0];
+                    if (!post.title) {
+                        postDb.updatePostTitle(post.id, messageText);
+                        sendTextMessage(senderID, "Please provide link of the post as well");
+                    } else if (!post.link) {
+                        postDb.updatePostLink(post.id, messageText);
+                        sendTextMessage(senderID, "Include some description as well. (Type SKIP to skip this step)");                        
+                    } else if (!post.description) {
+                        if (textChecker === 'skip') {
+                            postDb.updatePostDescription(post.id, "No description");                            
+                        } else {
+                            postDb.updatePostDescription(post.id, messageText);                            
                         }
-                    );
-                    break;
-                
-                default:
-                    const numberOfMeow = Math.floor(2 * Math.random()) + 1;
-                    let message = '';
-                    for (let i = 0; i < numberOfMeow; i++) {
-                        message += 'Meow' + ' ';
+                        sendTextMessage(senderID, "Latly, to improve engagement, please include ONE cover image for the post. (Type SKIP to skip this step)");                        
+                    } else if (!post.image) {
+                        if (textChecker === 'skip') {
+                            postDb.updatePostImage(post.id, DEFAULT_POST_IMAGE);
+                        } else {
+                            postDb.updatePostImage(post.id, messageText);                       
+                        }
+                        sendTextMessage(senderID, "Thank you. Your post is being recorded and broadcasted");
+                        userDb.updateUserIsPosting(senderID, 0); // reset isPosting state                                         
+                    } else {
+                        userDb.updateUserIsPosting(senderID, 0); // reset isPosting state                        
                     }
-                    sendTextMessage(senderID, message);
-                    break;
+                } else {
+                    userDb.updateUserIsPosting(senderID, 0); // reset isPosting state
+                }
+                return ;                
             }
         }
-    } else if (messageAttachments) {
-        console.log(messageAttachments);
-        switch (messageAttachments[0].type) {
-            case "image":
-                if (userStatus > 0) {
-                    messageDb.insertMessage(userStatus, messageAttachments[0].payload.url, messageDb.TYPE_IMAGE);
-                }
+        // Default action
+        switch (textChecker) {
+            case 'list':
+                sendList(senderID);
                 break;
+            
+            case 'image':
+                sendImageMessage(senderID);
+                break;
+            
+            case 'gif':
+                sendGifMessage(senderID);
+                break;
+            
+            case 'audio':
+                sendAudioMessage(senderID);
+                break;
+            
+            case 'video':
+                sendVideo(senderID);
+                break;
+            
+            case 'file':
+                sendFileMessage(senderID);
+                break;
+            
+            case 'menu':
+                showMenu(senderID);
+                break;
+            
+            case 'generic':
+                sendGenericMessage(senderID);
+                break;
+            
+            case 'more picture': 
+                sendMultipleImages(senderID);
+                break;
+
+            case 'event':
+                sendTextMessage(senderID, "https://www.facebook.com/events/419524075069645/");
+                break;
+            
+            case 'receipt':
+                sendReceiptMessage(senderID);
+                break;
+            
+            case 'quick reply':
+                sendQuickReply(senderID);
+                break;        
+            
+            case 'read receipt':
+                sendReadReceipt(senderID);
+                break;        
+            
+            case 'typing on':
+                sendTypingOn(senderID);
+                break;        
+            
+            case 'typing off':
+                sendTypingOff(senderID);
+                break;        
+            
+            case 'account linking':
+                sendAccountLinking(senderID);
+                break;
+            
+            case 'report':
+                console.log(event.message);
+                forwardMessage(senderID, event.message);
+                break;
+            
+            case 'show':
+                sendLatestPost(senderID);
+                break;
+
+            case 'show report':
+                sendLatestReport(senderID);
+                break;
+            
+            case 'hey':
+                getUserInfo(senderID).then(
+                    function (response) {
+                        sendTextMessage(senderID, "hey " + response["first_name"]);
+                    }
+                );
+                break;
+            
+            case 'post':
+                if (dbUser.role !== userDb.ROLE_MODERATOR) {
+                    break;
+                } else {
+                    postDb.insertPost(senderID);
+                    sendTextMessage("OK. You will need to first provide the title of the post.");
+                }
+            
             default:
-                const msg = "Message with attachment received."
-                sendTextMessage(senderID, msg);
+                showMenu(senderID);
+                break;
         }
+    } else if (messageAttachments) {
+        messageAttachments.forEach(function (messageAttachment) {
+            switch (messageAttachment.type) {
+                case "image":
+                    if (userStatus > 0) {
+                        messageDb.insertMessage(userStatus, messageAttachment.payload.url, messageDb.TYPE_IMAGE);
+                    }
+                    break;
+                default:
+                    const msg = "Message with attachment received."
+                    sendTextMessage(senderID, msg);
+            }
+        });
     }
 
     return ;
--- db/post.js
+++ db/post.js
@@ -15,15 +15,15 @@ function getLatestPost(limit) {
     return connection.queryAsync('SELECT * FROM post ORDER BY id DESC limit ?', [limit]);
 }
 
-// TODO: update this function to insert a new report
-function insertUser(facebookId, role) {
-    if (role === undefined) {
-         role = ROLE_USER;
-    }
+function getPostDetails(postId) {
+    return connection.queryAsync('SELECT * FROM post WHERE id = ?', [postId]);    
+}
+
+function insertPost(userId) {
     try {
         connection.queryAsync( 
-            "INSERT INTO `advocate`.`user` (`facebookId`, `role`) VALUES (?, ?);", 
-            [facebookId, role]
+            "INSERT INTO `advocate`.`post` (`userId`) VALUES (?);", 
+            [userId]
         );
         return "Insert Successful";
 
@@ -33,4 +33,62 @@ function insertUser(facebookId, role) {
     }
 }
 
+function updatePostTitle (postId, title) {
+    try {
+        connection.queryAsync( 
+            "UPDATE `advocate`.`post` SET `title` = ? WHERE `id` = ?",
+            [title, postId]
+        );
+        return "Update Successful";
+    } catch (ex){
+        console.log(ex);
+        throw ex;
+    }
+}
+
+function updatePostLink (postId, link) {
+    try {
+        connection.queryAsync( 
+            "UPDATE `advocate`.`post` SET `link` = ? WHERE `id` = ?",
+            [link, postId]
+        );
+        return "Update Successful";
+    } catch (ex){
+        console.log(ex);
+        throw ex;
+    }
+}
+
+function updatePostDescription (postId, description) {
+    try {
+        connection.queryAsync( 
+            "UPDATE `advocate`.`post` SET `description` = ? WHERE `id` = ?",
+            [description, postId]
+        );
+        return "Update Successful";
+    } catch (ex){
+        console.log(ex);
+        throw ex;
+    }
+}
+
+function updatePostImage (postId, imageUrl) {
+    try {
+        connection.queryAsync( 
+            "UPDATE `advocate`.`post` SET `imageUrl` = ? WHERE `id` = ?",
+            [imageUrl, postId]
+        );
+        return "Update Successful";
+    } catch (ex){
+        console.log(ex);
+        throw ex;
+    }
+}
+
 module.exports.getLatestPost = getLatestPost;
+module.exports.getPostDetails = getPostDetails;
+module.exports.insertPost = insertPost;
+module.exports.updatePostTitle = updatePostTitle;
+module.exports.updatePostLink = updatePostLink;
+module.exports.updatePostDescription = updatePostDescription;
+module.exports.updatePostImage = updatePostImage;
