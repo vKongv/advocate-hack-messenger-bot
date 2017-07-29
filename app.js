74bf6b24021038a131646598d6e9a73564b61437
duduvien <vivien@vkdevmy.com>
2017-07-29 11:47:26 +0800

    no message

--- app.js
+++ app.js
@@ -1058,12 +1058,11 @@ function sendEvent (recipientID) {
             type: "template",
             payload: {
                 template_type: "generic",
-                elements: [
-                    {
-                        title: "Test Event",
-                        item_url: "https://www.facebook.com/events/464835850549205/?acontext=%7B%22ref%22%3A%222%22%2C%22ref_dashboard_filter%22%3A%22upcoming%22%2C%22action_history%22%3A%22[%7B%5C%22surface%5C%22%3A%5C%22dashboard%5C%22%2C%5C%22mechanism%5C%22%3A%5C%22main_list%5C%22%2C%5C%22extra_data%5C%22%3A[]%7D]%22%7D",
-                        subtitle: "Event testing",
-                    },]
+                elements: [{
+                    title: "Event Test",
+                    item_url: "https://www.facebook.com/events/464835850549205/?acontext=%7B%22ref%22%3A%222%22%2C%22ref_dashboard_filter%22%3A%22upcoming%22%2C%22action_history%22%3A%22[%7B%5C%22surface%5C%22%3A%5C%22dashboard%5C%22%2C%5C%22mechanism%5C%22%3A%5C%22main_list%5C%22%2C%5C%22extra_data%5C%22%3A[]%7D]%22%7D",
+                    subtitle: "Event testing",
+                },],
             }
         }
     };
